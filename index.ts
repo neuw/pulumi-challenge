ff2fe8f4759dcc55d046e27959338d5d7898efbb
Karanbir Singh <krnbr@live.in>
2022-09-01 05:58:23 +0000

    Step 3 - setting up the website content

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as fs from "fs";
+import * as mime from "mime";
+const staticWebsiteDirectory = "website";
 
 const bucket = new aws.s3.BucketV2(
     "bucketV2",
@@ -14,4 +17,16 @@ const bucket = new aws.s3.BucketV2(
 const bucketAcl = new aws.s3.BucketAclV2("bAcl", {
     bucket: bucket.id,
     acl: aws.s3.PublicReadAcl,
+});
+
+fs.readdirSync(staticWebsiteDirectory).forEach((file) => {
+    const filePath = `${staticWebsiteDirectory}/${file}`;
+    const fileContent = fs.readFileSync(filePath).toString();
+
+    new aws.s3.BucketObject(file, {
+        bucket: bucket.id,
+        source: new pulumi.asset.FileAsset(filePath),
+        contentType: mime.getType(filePath) || undefined,
+        acl: aws.s3.PublicReadAcl,
+    });
 });
