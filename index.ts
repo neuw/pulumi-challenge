95effb3f9b09a95d3b2168226b20f2074d59f4ae
Karanbir Singh <krnbr@live.in>
2022-09-01 07:12:16 +0000

    Step 4 - 6

--- index.ts
+++ index.ts
@@ -1,32 +1,21 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import { CdnWebsite } from "./cdn-website";
+
+const website = new CdnWebsite("neuw", {});
+
+export const websiteUrl = website.url;
+
+import * as checkly from "@checkly/pulumi";
 import * as fs from "fs";
-import * as mime from "mime";
-const staticWebsiteDirectory = "website";
 
-const bucket = new aws.s3.BucketV2(
-    "bucketV2",
-    {
-        tags: {
-            Name: "My bucket",
-        },
-    }
-);
-
-const bucketAcl = new aws.s3.BucketAclV2("bAcl", {
-    bucket: bucket.id,
-    acl: aws.s3.PublicReadAcl,
-});
-
-fs.readdirSync(staticWebsiteDirectory).forEach((file) => {
-    const filePath = `${staticWebsiteDirectory}/${file}`;
-    const fileContent = fs.readFileSync(filePath).toString();
-
-    new aws.s3.BucketObject(file, {
-        bucket: bucket.id,
-        source: new pulumi.asset.FileAsset(filePath),
-        contentType: mime.getType(filePath) || undefined,
-        acl: aws.s3.PublicReadAcl,
-    });
+new checkly.Check("index-page", {
+    activated: true,
+    frequency: 10,
+    type: "BROWSER",
+    locations: ["ap-south-1"],
+    script: websiteUrl.apply((url) =>
+        fs
+            .readFileSync("checkly-embed.js")
+            .toString("utf8")
+            .replace("{{websiteUrl}}", url)
+    ),
 });
